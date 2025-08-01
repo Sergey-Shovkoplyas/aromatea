0595b2fe32e3fc8abc8f0c37cdef54e32f0734dc
Serhii Shovkoplias <serg@MacBook-Air-Serhii.local>
2025-08-01 21:38:48 +0300

    fixed ts-types

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,9 +3,10 @@ import Header from "@components/header/Header.tsx";
 const Home = () => {
   return (
     <div>
-      <Header />
-      <h1>Home page!!!</h1>
-    </div>
+      <Header/>
+      <h1>Home page</h1>
+
+     </div>
   );
 };
 
