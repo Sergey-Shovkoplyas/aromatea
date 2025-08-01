67ea88ee1dfafbb52558bf4a5134cd3908756f82
Serhii Shovkoplias <serg@MacBook-Air-Serhii.local>
2025-08-01 20:26:17 +0300

    fix ts and add header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './Header.styles'
+
+const Head = () => {
+  return (
+    <S.Root>
+      HeaderValue
+    </S.Root>
+  );
+};
+
+export default Head;
