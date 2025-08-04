40110c47ccb8a6f123522437d0a6309f108c7bcb
Serhii Shovkoplias <serg@MacBook-Air-Serhii.local>
2025-08-04 13:18:02 +0300

    add redirect to default page

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,15 +1,21 @@
-import { Route, Routes as ReactRoutes } from 'react-router';
+import { Navigate, Route, Routes as ReactRoutes } from 'react-router';
 
 import About from '@pages/about/About.tsx';
 import Contact from '@pages/contact/Contact.tsx';
 import Home from '@pages/home/Home.tsx';
+import useDetectLocale from '@hooks/useDetectLocale.ts';
 
 const Routes = () => {
+  const locale = useDetectLocale();
+
   return (
     <ReactRoutes>
       <Route path="/:locale/" element={<Home />} />
       <Route path="/:locale/about" element={<About />} />
       <Route path="/:locale/contact" element={<Contact />} />
+
+      {/*Redirect if no route matches*/}
+      <Route path="*" element={<Navigate to={`/${locale}/`} replace />} />
     </ReactRoutes>
   );
 };
